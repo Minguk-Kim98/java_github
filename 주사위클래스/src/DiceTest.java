78115032133f119f3f60c724516466fa6e9d7dfa
Minguk-Kim98 <minguk1097@naver.com>
2021-06-22 17:39:13 +0900

    랜덤으로 주사위 굴리기

---
+++ 주사위클래스/src/DiceTest.java
@@ -0,0 +1,14 @@
+class Dice {
+	int face;
+	public int roll() {
+		face = (int)(Math.random()*6 + 1);
+		return face;
+	}
+	
+}
+public class DiceTest {
+	public static void main(String[] args) {
+		Dice d = new Dice();
+		System.out.println("주사위의 숫자 : " + d.roll());
+	}
+}
